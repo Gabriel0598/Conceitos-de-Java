bf74f05f753a655c913f840b50ed0df9464cdfee
Gabriel <augustogabriel35@gmail.com>
2021-07-09 01:45:17 -0300

    Aula 202 (Soma de numeros impares em um intervalo)

---
+++ Soma_impares.java
@@ -0,0 +1,40 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Soma_impares
+{
+
+	public static void main(String[] args)
+	{
+		//Aula 202
+		Locale.setDefault(Locale.US);
+		Scanner sc8 = new Scanner(System.in);
+		
+		int x, y, troca, soma;
+		
+		System.out.println("Digite dois números: ");
+		x = sc8.nextInt();
+		y = sc8.nextInt();
+		
+		if (x > y)
+		{
+			troca = x;
+			x = y;
+			y = troca;
+		}
+		
+		soma = 0;
+		for (int i = x+1; i < y; i++)
+		{
+			if(i % 2 != 0) //Se é divísil por 2 logo é par, por isso usar mod 2;
+			{
+				soma = soma + i;
+			}
+		}
+		
+		System.out.println("SOMA DOS IMPARES = " + soma);
+		
+		sc8.close();
+	}
+
+}
