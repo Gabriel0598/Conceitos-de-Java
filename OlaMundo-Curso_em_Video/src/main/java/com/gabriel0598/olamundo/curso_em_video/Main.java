cc529e07e3ca6a02715be327c1fa997b82703b8b
Gabriel <augustogabriel35@gmail.com>
2021-09-10 23:16:16 -0300

    C2 - Alteracao de sintaxe incorreta no arquivo aula01

--- OlaMundo-Curso_em_Video/src/main/java/com/gabriel0598/olamundo/curso_em_video/Main.java
+++ OlaMundo-Curso_em_Video/src/main/java/com/gabriel0598/olamundo/curso_em_video/Main.java
@@ -9,6 +9,6 @@ public class Main
     public static void main(String [] args)
     {
         //
-        System.out.prinln("Hello World!!!");
+        System.out.println("Hello World!!!");
     }
 }
