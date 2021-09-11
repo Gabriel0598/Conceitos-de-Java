7bafc441aff68d4ce3b5015af387dddd1f8f046f
Gabriel <augustogabriel35@gmail.com>
2021-09-10 23:10:37 -0300

    C1 - Commit inicial aula01

---
+++ OlaMundo-Curso_em_Video/src/main/java/com/gabriel0598/olamundo/curso_em_video/Main.java
@@ -0,0 +1,14 @@
+//Curso em vídeo - Aula Prat. 2
+package com.gabriel0598.olamundo.curso_em_video;
+
+/*
+    @author gabri
+*/
+public class Main
+{
+    public static void main(String [] args)
+    {
+        //
+        System.out.prinln("Hello World!!!");
+    }
+}
