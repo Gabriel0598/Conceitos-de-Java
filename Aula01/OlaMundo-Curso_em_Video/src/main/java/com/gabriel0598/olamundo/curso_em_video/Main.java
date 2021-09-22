ffe64e82a38f7c20c6301304a9917c8f4159fb98
Gabriel <augustogabriel35@gmail.com>
2021-09-22 13:17:43 -0300

    C3 - Migracao de pasta/ Comentarios

---
+++ Aula01/OlaMundo-Curso_em_Video/src/main/java/com/gabriel0598/olamundo/curso_em_video/Main.java
@@ -0,0 +1,11 @@
+//Curso em vídeo - Aula 1 - Prática
+package com.gabriel0598.olamundo.curso_em_video;
+
+public class Main
+{
+    public static void main(String [] args)
+    {
+        //Saída de texto por "Hello World";
+        System.out.println("Hello World!!!");
+    }
+}
