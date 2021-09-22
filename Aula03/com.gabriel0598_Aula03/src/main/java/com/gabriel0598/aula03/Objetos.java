9155190595fa5342980d90c03760e94b16bd3576
Gabriel <augustogabriel35@gmail.com>
2021-09-22 18:21:45 -0300

    C7 - Instanciacao do objeto caneta

---
+++ Aula03/com.gabriel0598_Aula03/src/main/java/com/gabriel0598/aula03/Objetos.java
@@ -0,0 +1,14 @@
+package com.gabriel0598.aula03;
+
+public class Objetos
+{
+    public static void main(String[] args)
+    {
+        //Sout + Tab = autocompleta system.out.prinln();
+        //Instanciação de objeto:
+        //Sintaxe = Classe nomeObjeto = new Classe ();
+        Caneta c1 = new Caneta();
+        c1.status();
+        //Status para indicar o que está armezenado;
+    }
+}
