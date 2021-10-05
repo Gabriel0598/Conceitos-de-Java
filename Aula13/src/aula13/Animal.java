c515a62cf35dca4301dce526ed7d8532204d24de
Gabriel <augustogabriel35@gmail.com>
2021-10-05 02:38:57 -0300

    C45 - Escopo de animal

---
+++ Aula13/src/aula13/Animal.java
@@ -0,0 +1,9 @@
+package aula13;
+//Escopo de animal
+public abstract class Animal
+{
+    protected float peso;
+    protected int idade;
+    protected int membro;
+    public abstract void emitirSom();
+}
